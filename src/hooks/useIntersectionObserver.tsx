73115bccddf909aeac8669120967da6fc86fc62b
Arvind-t33 <arvindtawker@gmail.com>
2024-11-22 21:24:25 -0800

    added this amazing vanish feature based on observer interaction

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -4,11 +4,21 @@ import { useEffect, useState, useRef } from 'react';
 export const useIntersectionObserver = () => {
   const [isVisible, setIsVisible] = useState(false);
   const ref = useRef<HTMLDivElement>(null);
+  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
       ([entry]) => {
-        setIsVisible(entry.isIntersecting);
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          if (timeoutIdRef.current) {
+            clearTimeout(timeoutIdRef.current);
+          }
+        } else {
+          timeoutIdRef.current = setTimeout(() => {
+            setIsVisible(false);
+          },1000); // 8 seconds delay
+        }
       },
       {
         threshold: Array.from({ length: 11 }, (_, i) => i * 0.1), // Adjust this value as needed
@@ -22,6 +32,9 @@ export const useIntersectionObserver = () => {
     return () => {
       if (ref.current) {
         observer.unobserve(ref.current);
+      }
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
       }
     };
   }, []);
