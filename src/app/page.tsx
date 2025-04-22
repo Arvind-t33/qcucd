d9b3c1733e52c76331c202b765ae72ab5195c0e3
Arvind-t33 <arvindtawker@gmail.com>
2025-04-21 21:38:43 -0700

    navbar moved to layout.tsx for universal rendering across all pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,8 @@
 import { HomeContent } from "@/components/HomeContent";
-import { Navbar } from "@/components/ui/Navbar";
-import { NavbarSolid } from "@/components/ui/NavbarSolid";
-
-const navbarItems = [
-  { text: "Home", href: "/" },
-  { text: "About", href: "/about" },
-  { text: "Workshops", href: "/workshops" },
-  { text: "Contact", href: "/contact" },
-  { text: "Join", href: "/join" },
-];
 
 export default function Home() {
   return (
     <div className="min-h-screen pt-16 sm:pt-20 p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Navbar items={navbarItems}/>
-      <NavbarSolid items={navbarItems}/>
       <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center">
         <HomeContent />
       </div>
