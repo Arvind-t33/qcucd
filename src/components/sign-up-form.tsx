868c2ebdeb0665eece06e4d6c403dd0ff0340390
Arvind-t33 <arvindtawker@gmail.com>
2025-04-21 21:09:55 -0700

    commented out the form input text boxes as discord link is the only entry point

--- src/components/sign-up-form.tsx
+++ src/components/sign-up-form.tsx
@@ -23,7 +23,7 @@ export function SignupFormDemo() {
       </div>
 
       <form className="my-8" onSubmit={handleSubmit}>
-        <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
+        {/* <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-2 mb-4">
           <LabelInputContainer>
             <Label htmlFor="firstname" className="text-white">First name</Label>
             <Input id="firstname" placeholder="Tyler" type="text" />
@@ -36,7 +36,7 @@ export function SignupFormDemo() {
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email" className="text-white">Email Address</Label>
           <Input id="email" placeholder="projectmayhem@fc.com" type="email" />
-        </LabelInputContainer>
+        </LabelInputContainer> 
 
         <button
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
@@ -44,7 +44,7 @@ export function SignupFormDemo() {
         >
           Sign up &rarr;
           <BottomGradient />
-        </button>
+        </button> */}
 
         <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
 
